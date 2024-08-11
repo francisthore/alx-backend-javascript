52e7be299fd2932b470f237b6ed6184e9fe43b97
Francis Thore <thorefrancis@gmail.com>
2024-08-11 22:18:00 +0200

    reject then

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,16 +1,13 @@
 // Handles a promise object
 export default function handleResponseFromAPI(promise) {
-  promise.then(
-    (result) => {
+  return promise
+    .then(() => {
       const resultObject = {
         status: 200,
         body: 'Success',
       };
       console.log('Got a response from the API');
       return resultObject;
-    },
-    (error) => {
-      return {};
-    }
-  );
+    })
+    .catch(() => new Error());
 }
