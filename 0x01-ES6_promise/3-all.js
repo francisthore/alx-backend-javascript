f29707000ae9c1ed8a23d73b8c46ec9b98d498e0
Francis Thore <thorefrancis@gmail.com>
2024-08-11 23:12:27 +0200

    now?

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,9 +1,8 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  const promise = Promise.all([uploadPhoto(), createUser()]);
-  promise
-    .then((res) => {
-      console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
-    });
+  return Promise.all([uploadPhoto(), createUser()]
+    .then(([photoRes, userRes]) => {
+      console.log(`${photoRes.body} ${userRes.firstName} ${userRes.lastName}`);
+    }));
 }
