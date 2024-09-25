540b17f35ada372a4b4c0f56f6c0532d782c59ef
Francis Thore <thorefrancis@gmail.com>
2024-09-26 00:06:23 +0200

    err

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,15 +1,12 @@
 export default class Building {
   constructor(sqft) {
+    if (typeof this.evacuationWarningMessage !== 'function') {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
     this._sqft = sqft;
   }
 
   get sqft() {
     return this._sqft;
   }
-
-  /* eslint-disable class-methods-use-this */
-  evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
-  }
-  /* eslint-disable class-methods-use-this */
 }
