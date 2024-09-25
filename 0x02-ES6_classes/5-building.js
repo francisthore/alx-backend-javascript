5e49b9ed9e5c8ae89946868d639bf0c4911f3d9d
Francis Thore <thorefrancis@gmail.com>
2024-09-26 00:11:38 +0200

    check if construct is of another class

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,7 +1,9 @@
 export default class Building {
   constructor(sqft) {
-    if (typeof this.evacuationWarningMessage !== 'function') {
-      throw new Error('Class extending Building must override evacuationWarningMessage');
+    if (this.constructor !== Building) {
+      if (typeof this.evacuationWarningMessage !== 'function') {
+        throw new Error('Class extending Building must override evacuationWarningMessage');
+      }
     }
     this._sqft = sqft;
   }
