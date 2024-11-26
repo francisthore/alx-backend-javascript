adefa0a37155858e25765a207e5210947896dc6a
Francis Thore <thorefrancis@gmail.com>
2024-11-26 23:04:58 +0200

    revert 4 further

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -6,11 +6,7 @@ const http = require('http');
 const app = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  if (req.url === '/') {
-    res.end('Hello Holberton School!');
-  } else if (req.url === '/students') {
-    res.end('Hey students');
-  }
+  res.end('Hello Holberton School!');
 });
 
 app.listen(1245);
