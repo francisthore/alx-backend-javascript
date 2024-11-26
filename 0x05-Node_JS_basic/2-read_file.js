175e10881cb8eed6d14f94239ffa289d06cba61e
Francis Thore <thorefrancis@gmail.com>
2024-11-26 19:34:33 +0200

    read csv sync

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,36 @@
+/**
+ * Opens a csv and reads
+ */
+const fs = require('fs');
+
+const countStudents = (filePath) => {
+  try {
+    const fieldInfo = {};
+    const data = fs.readFileSync(filePath, { encoding: 'utf8' });
+    const rows = data.split('\n').filter((row) => row.trim() !== '');
+    rows.shift();
+    const totalStudents = rows.length;
+    console.log('Number of students', totalStudents);
+    rows.forEach((row) => {
+      const [firstName, , , field] = row.split(',');
+
+      if (!fieldInfo[field]) {
+        fieldInfo[field] = {
+          count: 0,
+          names: [],
+        };
+      }
+      fieldInfo[field].count += 1;
+      fieldInfo[field].names.push(firstName);
+    });
+
+    Object.keys(fieldInfo).forEach((field) => {
+      const { count, names } = fieldInfo[field];
+      console.log(`Number of students in ${field}: ${count}. List: ${names.join(', ')}`);
+    });
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+};
+
+module.exports = countStudents;
