c33b284d177b52115bcbb64a2b827e309a0edef0
Francis Thore <thorefrancis@gmail.com>
2024-11-26 22:14:59 +0200

    http

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,14 @@
+/**
+ * Sets up basic http server
+ */
+const http = require('http');
+
+const app = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello Holberton School!');
+});
+
+app.listen(1245);
+
+module.exports = app;
