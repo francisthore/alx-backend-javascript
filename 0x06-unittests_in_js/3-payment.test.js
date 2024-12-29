958bdb26d3b33c44f906a2cb6407d38fa84a205a
Francis Thore <thorefrancis@gmail.com>
2024-12-30 00:18:46 +0200

    sin

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -5,11 +5,11 @@ const sendPaymentRequestToApi = require('./3-payment');
 
 describe('sendPaymentRequestToApi', () => {
   it('sendPaymentRequestToApi uses the calculateNumber method of Utils', () => {
-    const bigBrother = sinon.spy(Utils);
+    const theSin = sinon.spy(Utils);
 
     sendPaymentRequestToApi(100, 20);
-    expect(bigBrother.calculateNumber.calledWith('SUM', 100, 20)).to.be.true;
-    expect(bigBrother.calculateNumber.callCount).to.be.equal(1);
-    bigBrother.calculateNumber.restore();
+    expect(theSin.calculateNumber.calledWith('SUM', 100, 20)).to.be.true;
+    expect(theSin.calculateNumber.callCount).to.be.equal(1);
+    theSin.calculateNumber.restore();
   });
 });
